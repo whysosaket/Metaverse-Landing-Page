d71dac7c81bc2920214739a2f914871c2fab0c29
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-09-21 11:09:06 +0530

    Configuration

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-4xl">Hello World</h1>
+      <h1 className="text-4xl text-primary font-inter font-bold">Hello World</h1>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F195F0',
+        secondary: '#FA56F7',
+      },
+      fontFamily: {
+        inter: ["Inter", "system-ui"],
+      }
+    },
   },
   plugins: [],
 }
