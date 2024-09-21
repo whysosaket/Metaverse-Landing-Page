0afbe6492ecf5b9001c48d21525ba367bdaa121a
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-09-21 13:46:10 +0530

    Stats Relative Fix

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -4,8 +4,8 @@ import Inter from "./shared/Inter";
 const Stats = () => {
   return (
     <div className="flex justify-between align-middle my-20">
-      <div className="w-6/12">
-        <div className="text-[3.2rem] my-4 font-universo text-justify leading-none">
+      <div className="w-5/12">
+        <div className="text-[2.6rem] my-4 font-universo text-justify leading-tight">
           Explore the Virtual <GradientText text="World." />
         </div>
         <p className="text-gray-400 text-justify">
@@ -13,7 +13,7 @@ const Stats = () => {
           world using VR and AR with a free service.
         </p>
       </div>
-      <div className="w-1/3 flex justify-center gap-10 font-universothin my-auto">
+      <div className="w-1/3 flex justify-center gap-10 font-universothin mt-2">
         <div className="w-1/3">
             <h1 className="font-semibold text-5xl my-2"><Inter text="43K+" /></h1>
             <p className="text-gray-400">Liason partner to collaborate</p>
