3a64d361495cf0634e01501be1898f94ceab6237
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-09-21 13:36:47 +0530

    Delete Button.tsx

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-const Button = (props: { text: string }) => {
-  return (
-    <div className="bg-custom-gradient flex align-middle text-black hover:bg-custom-gradient2 px-6 py-3 rounded-3xl font-semibold text-center select-none cursor-pointer">
-      <span className="my-auto">{props.text}</span>
-    </div>
-  );
-};
-
-export default Button;
